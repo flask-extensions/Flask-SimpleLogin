41740bdc7c4959bdd6844ab9568fb03be5388fbc
Justin Forseth <51516525+jforseth210@users.noreply.github.com>
2020-09-10 09:55:28 -0600

    Code style improvement
    Co-authored-by: Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -33,7 +33,7 @@ def test_post_with_token(client):
     # token is still invalid :(
 
 def test_no_message_customization(client):
-    response = client.get('/secret', follow_redirects = True)
+    response = client.get('/secret', follow_redirects=True)
     assert response.status_code == 200
     assert b'You need to login first' in response.data
 
