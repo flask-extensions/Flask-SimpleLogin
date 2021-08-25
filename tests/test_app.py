16483d0be1db144fd48c6205e458e132ab45b93f
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-08-25 12:38:51 -0400

    Fixes CSRF tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
-from flask import url_for, session
+from flask import url_for
 
-# from flask_simplelogin import is_logged_in
+from flask_simplelogin import is_logged_in
+
+
+CSRF_TOKEN = "IjEwN2Q4ZjZlNWI5NjM5NjRhODk3Yzg0NzZmM2QyYjZhNzNiNDY0OWMi.YSZw7A.IUjf7OkrrCmCcg4IaV92MIrKjQ0"
+CSRF_TOKEN_HASH = "107d8f6e5b963964a897c8476f3d2b6a73b4649c"
 
 
 def test_get_login(client):
@@ -18,15 +22,20 @@ def test_post_requires_token(client):
 
 
 def test_post_with_token(client):
-    session.clear()
-    session["csrf_token"] = "123456"
+    with client.session_transaction() as session:
+        session["csrf_token"] = CSRF_TOKEN_HASH
+
+    client.get(url_for("simplelogin.login"))  # creates CSRF token
     response = client.post(
         url_for("simplelogin.login"),
-        data={"username": "admin", "password": "secret", "csrf_token": "123456"},
+        data={
+            "username": "admin",
+            "password": "secret",
+            "csrf_token": "not a CSRF token",
+        },
     )
     assert response.status_code == 200
     assert "csrf_token The CSRF token is missing" not in str(response.data)
-    # token is still invalid :(
 
 
 def test_negative_redirect_to_external_url(client):
@@ -48,17 +57,18 @@ def test_positive_redirect_to_allowed_host(app):
         assert response.status_code == 200
 
 
-# def test_is_logged_in(client):
-#     session.clear()
-#     session['csrf_token'] = '123456'
-#     response = client.post(
-#         url_for('simplelogin.login'),
-#         data={
-#             'username': 'admin',
-#             'password': 'secret',
-#             'csrf_token': '123456'
-#         }
-#     )
-#     assert response.status_code == 200
-#     print(response.data)
-#     # assert is_logged_in() is True
+def test_is_logged_in(client):
+    with client.session_transaction() as session:
+        session["csrf_token"] = CSRF_TOKEN_HASH
+
+    assert not is_logged_in()
+    response = client.post(
+        url_for("simplelogin.login"),
+        data={
+            "username": "admin",
+            "password": "secret",
+            "csrf_token": CSRF_TOKEN,
+        },
+    )
+    assert response.status_code == 302
+    assert is_logged_in()
